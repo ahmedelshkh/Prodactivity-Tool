56d0ae28a2708452657bfd72d5ffdd81f06b28f4
ahmedelshkh <ahmedelshkh6566@gmail.com>
2025-02-14 00:00:05 +0200

    finished the timer functionality

--- script.js
+++ script.js
@@ -42,3 +42,71 @@ menu.onclick = function () {
     toggleLines[2].classList.remove("line3-rotate");
     click = true;
 }
+
+// Start Timer Functionality
+
+let timerText = document.querySelector(".home .circle .time");
+let timerMinutes = 30;
+let timerSeconds = 0;
+let allTime = timerMinutes * 60 + timerSeconds;
+let allTimeNow = allTime;
+let startBtn = document.querySelector(".home .control-buttons .start-btn");
+let editBtn = document.querySelector(".home .control-buttons .edit-btn");
+let prograssPar = document.querySelector("#progress");
+let timerStart = false;
+
+startBtn.addEventListener("click", function() {
+    if(timerStart){
+        startBtn.textContent = "Start";
+        timerStart = false;
+        stopTimer();
+    }else {
+        startTimer();
+        startBtn.textContent = "Stop";
+        timerStart = true;
+    }
+});
+
+function startTimer(stop) {
+    if (stop) {
+        console.log("stoped");
+        clearInterval(timerInterval);
+    }else {
+        timerInterval = setInterval(() => {
+        countTime();
+        if(timerMinutes < 10 && timerSeconds < 10) {
+            timerText.textContent = `0${timerMinutes}:0${timerSeconds}`;
+        } else if (timerMinutes < 10) {
+            timerText.textContent = `0${timerMinutes}:${timerSeconds}`;
+        } else if (timerSeconds < 10) {
+            timerText.textContent = `${timerMinutes}:0${timerSeconds}`;
+        } else {
+            timerText.textContent = `${timerMinutes}:${timerSeconds}`;
+        }
+        
+    }, 1000);
+    };
+    
+};
+
+function countTime() {
+    if(timerMinutes < 1 && timerSeconds < 1) {
+        timerCompleted();
+    }
+    if(timerSeconds < 1) {
+        timerMinutes -= 1;
+        timerSeconds = 59;
+    }else {
+        timerSeconds -= 1;
+    };
+    prograssPrecentege();
+};
+function stopTimer() { 
+    startTimer(true);
+};
+function prograssPrecentege() {
+    allTimeNow = timerMinutes * 60 + timerSeconds;;
+    let precentege = allTimeNow / allTime;
+    let prograssPos = precentege * 280;
+    prograssPar.style.strokeDashoffset = prograssPos;
+}
